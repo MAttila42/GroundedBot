1ca75f7aea1a5646521290f06769553b3cc2ae0a
ExAtom <menyhart.atika@gmail.com>
2021-01-20 13:11:51 +0100

    Better C# eval

--- Commands/Dev/Evaluate.cs
+++ Commands/Dev/Evaluate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Timers;
 using Microsoft.Scripting;
 using Microsoft.Scripting.Hosting;
+using CodingSeb.ExpressionEvaluator;
 using IronPython.Hosting;
 using Discord;
 using GroundedBot.Json;
@@ -50,7 +51,9 @@ namespace GroundedBot.Commands
                 switch (m[1])
                 {
                     case "cs":
-                        result = Z.Expressions.Eval.Execute(code).ToString();
+                        //result = Z.Expressions.Eval.Execute(code).ToString();
+                        ExpressionEvaluator csEvaluator = new ExpressionEvaluator();
+                        result = csEvaluator.ScriptEvaluate(code).ToString();
                         break;
                     case "py":
                         var py = new PythonScript();

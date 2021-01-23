6c98d38d33fb9638d5283559f58c1b5fd6b7d826
ExAtom <menyhart.atika@gmail.com>
2021-01-23 14:37:17 +0100

    Updated Discord.Net

--- Events/RemoveWhoLeft.cs
+++ Events/RemoveWhoLeft.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using GroundedBot.Json;
+﻿using GroundedBot.Json;
 
 namespace GroundedBot.Events
 {
     class RemoveWhoLeft
     {
-        public static async void DoEvent(ulong id)
+        public static void DoEvent(ulong id)
         {
             var members = Members.PullData();
             members.RemoveAt(members.IndexOf(members.Find(x => x.ID == id)));

ddfac67dca7b5fab453ec3a2cb85b5d42730bce8
ExAtom <menyhart.atika@gmail.com>
2021-02-07 15:29:34 +0100

    Test command tidying up

--- Commands/Dev/Test.cs
+++ Commands/Dev/Test.cs
@@ -33,7 +33,7 @@ namespace GroundedBot.Commands
             {
                 switch (message.Content.Split()[1].ToLower())
                 {
-                    case "helpfloppy":
+                    case "helpfloppy": // Give out the floppies based on the amount of help
                         for (int i = 0; i < members.Count; i++)
                         {
                             members[i].Floppy += members[i].Help;
@@ -44,102 +44,13 @@ namespace GroundedBot.Commands
                         await message.Channel.SendMessageAsync("Done.");
                         return;
 
-                    case "removewholeft":
-                        var lefts = new List<Members>();
-                        foreach (var i in members)
-                            try
-                            {
-                                if (Program._client.GetUser(i.ID) == null)
-                                    lefts.Add(i);
-                            }
-                            catch (Exception) { lefts.Add(i); }
-                        foreach (var i in lefts)
-                            members.Remove(i);
-                        Members.PushData(members);
-                        await message.Channel.SendMessageAsync("Done.");
-                        return;
-
-                    case "revertdata":
-                        var ranks = new List<string>(File.ReadAllLines("ranks.txt"));
-                        var floppies = new List<string>(File.ReadAllLines("floppies.txt"));
-                        var ids = new HashSet<ulong>();
-
-                        foreach (var i in ranks)
-                            try { ids.Add(Program.GetUserId(i.Split('&')[0])); }
-                            catch (Exception) { }
-                        foreach (var i in floppies)
-                            try { ids.Add(Program.GetUserId(i.Split()[0])); }
-                            catch (Exception) { }
-                        foreach (var i in ids)
-                            if (i != 0)
-                                members.Add(new Members(i));
-
-                        foreach (var i in ranks)
-                        {
-                            try
-                            {
-                                var memberIndex = members.IndexOf(members.Find(x => x.ID == Program.GetUserId(i.Split('&')[0])));
-                                if (memberIndex == -1)
-                                    continue;
-
-                                var rank = int.Parse(i.Split('&')[1]);
-                                int rankup = 30;
-                                int xp = 0;
-                                for (int j = 0; j < rank; j++)
-                                {
-                                    xp += rankup;
-                                    rankup += rankup / 5;
-                                }
-
-                                members[memberIndex].XP = xp;
-                                members[memberIndex].Rank = rank;
-                                members[memberIndex].Floppy += rank;
-                            }
-                            catch (Exception) { }
-                        }
-
-                        foreach (var i in floppies)
-                        {
-                            try
-                            {
-                                var memberIndex = members.IndexOf(members.Find(x => x.ID == ulong.Parse(i.Split()[0])));
-                                if (memberIndex == -1)
-                                    continue;
-                                var floppy = int.Parse(i.Split()[1]);
-                                members[memberIndex].Floppy += floppy * 10;
-                            }
-                            catch (Exception) { }
-                        }
-
-                        Members.PushData(members);
-                        await message.Channel.SendMessageAsync("Done.");
-
-                        return;
-
-                    case "searchhelps":
+                    case "searchhelps": // List of all the members who helped this month
                         foreach (var i in members.Where(x => x.Help > 0))
                             await message.Channel.SendMessageAsync($"{Program._client.GetUser(i.ID).Mention} - {i.Help}");
-                        await message.Channel.SendMessageAsync("Done.");
+                        await message.Channel.SendMessageAsync("Done.", allowedMentions: AllowedMentions.None);
                         return;
 
-                    case "searchlasthelps":
-                        foreach (var i in members.Where(x => x.LastHelp > 0))
-                            await message.Channel.SendMessageAsync($"{Program._client.GetUser(i.ID).Mention} - {i.LastHelp}");
-                        await message.Channel.SendMessageAsync("Done.");
-                        return;
-
-                    case "revertlasthelps":
-                        foreach (var i in File.ReadAllLines("lasthelps.txt"))
-                        {
-                            ulong id = ulong.Parse(i.Split()[0]);
-                            members.Add(new Members(id));
-                            members[members.IndexOf(members.Find(x => x.ID == id))].LastHelp = int.Parse(i.Split()[1]);
-                        }
-                        Members.PushData(members);
-                        await message.Channel.SendMessageAsync("Done.");
-                        return;
-
-                    case "removeduplicates":
+                    case "removeduplicates": // Remove duplicated objects from the database
                         var fixedMembers = new List<Members>();
                         foreach (var i in members)
                             if (fixedMembers.Find(x => x.ID == i.ID) == null)
@@ -148,7 +59,7 @@ namespace GroundedBot.Commands
                         await message.Channel.SendMessageAsync("Done.");
                         return;
 
-                    case "getptan+date":
+                    case "getaptan+date": // Puts the current date as the expiry date of Ptan+
                         members[Members.GetMemberIndex(members, message.Author.Id.ToString())].PPlusDate = DateTime.Now.ToString("dd/MM/yyyy");
                         Members.PushData(members);
                         await message.Channel.SendMessageAsync("Done.");

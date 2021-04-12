47eef6c2ad5886204d9fae82750e8d48f07910b1
ExAtom <menyhart.atika@gmail.com>
2021-04-12 15:53:00 +0200

    Fixing some issues

--- Events/BotMention.cs
+++ Events/BotMention.cs
@@ -16,37 +16,39 @@ namespace GroundedBot.Events
         {
             var message = Recieved.Message;
             ulong id = Program._client.CurrentUser.Id;
+            if (!((message.Content == $"<@!{id}>" || message.Content == $"<@{id}>") && Program.BotChannel()))
+                return;
             char prefix = BaseConfig.GetConfig().Prefix;
-            string url = "https://api.github.com/repos/ExAtom/GroundedBot/contributors";
-            JsonDocument Response;
+            //string url = "https://api.github.com/repos/ExAtom/GroundedBot/contributors";
+            //JsonDocument Response;
 
-            using (var httpClient = new HttpClient())
-            {
-                using (var request = new HttpRequestMessage(new HttpMethod("GET"), url))
-                {
-                    request.Headers.TryAddWithoutValidation("Accept", "application/vnd.github.v3+json");
+            //using (var httpClient = new HttpClient())
+            //{
+            //    using (var request = new HttpRequestMessage(new HttpMethod("GET"), url))
+            //    {
+            //        request.Headers.TryAddWithoutValidation("Accept", "application/vnd.github.v3+json");
 
-                    request.Headers.UserAgent.Add(new ProductInfoHeaderValue("GroundedBot", "1.0"));
-                    request.Headers.UserAgent.Add(new ProductInfoHeaderValue("(+https://github.com/ExAtom/GroundedBot)"));
+            //        request.Headers.UserAgent.Add(new ProductInfoHeaderValue("GroundedBot", "1.0"));
+            //        request.Headers.UserAgent.Add(new ProductInfoHeaderValue("(+https://github.com/ExAtom/GroundedBot)"));
 
-                    var response = await httpClient.SendAsync(request);
-                    using (StreamReader reader = new StreamReader(response.Content.ReadAsStream()))
-                    {
-                        Response = JsonDocument.Parse(reader.ReadToEnd());
-                    }
-                }
-            }
+            //        var response = await httpClient.SendAsync(request);
+            //        using (StreamReader reader = new StreamReader(response.Content.ReadAsStream()))
+            //        {
+            //            Response = JsonDocument.Parse(reader.ReadToEnd());
+            //        }
+            //    }
+            //}
 
-            var rootElement = Response.RootElement;
-            var enumerator = rootElement.EnumerateArray();
-            List<EmbedFieldBuilder> fieldBuilders = new List<EmbedFieldBuilder>();
+            //var rootElement = Response.RootElement;
+            //var enumerator = rootElement.EnumerateArray();
+            //List<EmbedFieldBuilder> fieldBuilders = new List<EmbedFieldBuilder>();
 
-            foreach (var idk in enumerator)
-            {
-                fieldBuilders.Add(new EmbedFieldBuilder().WithIsInline(false)
-                    .WithName(idk.GetProperty("login").ToString())
-                    .WithValue(idk.GetProperty("html_url").ToString()));
-            }
+            //foreach (var idk in enumerator)
+            //{
+            //    fieldBuilders.Add(new EmbedFieldBuilder().WithIsInline(false)
+            //        .WithName(idk.GetProperty("login").ToString())
+            //        .WithValue(idk.GetProperty("html_url").ToString()));
+            //}
 
             var infoEmbed = new EmbedBuilder()
                 .WithAuthor(author =>
@@ -63,28 +65,25 @@ namespace GroundedBot.Events
                 .WithFooter(((SocketGuildChannel)message.Channel).Guild.Name)
                 .WithColor(new Color(0x7289DA)).Build();
 
-            var contEmbed = new EmbedBuilder()
-                .WithAuthor(author =>
-                {
-                    author
-                        .WithName("Contributors of the GroundedBot project")
-                        .WithIconUrl("https://cdn.discordapp.com/attachments/782305154342322226/801852346350174208/noun_Information_405516.png"); // Information by Viktor Ostrovsky from the Noun Project
-                })
-                .WithFields(fieldBuilders)
-                .WithFooter(((SocketGuildChannel)message.Channel).Guild.Name)
-                .WithColor(new Color(0x7289DA)).Build();
+            //var contEmbed = new EmbedBuilder()
+            //    .WithAuthor(author =>
+            //    {
+            //        author
+            //            .WithName("Contributors of the GroundedBot project")
+            //            .WithIconUrl("https://cdn.discordapp.com/attachments/782305154342322226/801852346350174208/noun_Information_405516.png"); // Information by Viktor Ostrovsky from the Noun Project
+            //    })
+            //    .WithFields(fieldBuilders)
+            //    .WithFooter(((SocketGuildChannel)message.Channel).Guild.Name)
+            //    .WithColor(new Color(0x7289DA)).Build();
 
-            if ((message.Content == $"<@!{id}>" || message.Content == $"<@{id}>") && Program.BotChannel())
-            {
-                await message.Channel.SendMessageAsync(
-                    null,
-                    embed: infoEmbed)
-                    .ConfigureAwait(false);
-                await message.Channel.SendMessageAsync(
-                    null,
-                    embed: contEmbed)
-                    .ConfigureAwait(false);
-            }
+            await message.Channel.SendMessageAsync(
+                null,
+                embed: infoEmbed)
+                .ConfigureAwait(false);
+            //await message.Channel.SendMessageAsync(
+            //    null,
+            //    embed: contEmbed)
+            //    .ConfigureAwait(false);
         }
     }
 }

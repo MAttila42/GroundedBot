9c2da5a8b092024096dd7fde0e60668a63f2a759
Menyhárt Attila <menyhart.atika@gmail.com>
2022-10-26 19:07:39 +0200

    Bugfixes
    - Students leaving guild caused errors -- should be fixed now

--- Commands/Dev.cs
+++ Commands/Dev.cs
@@ -1,84 +1,61 @@
 ﻿using System.Diagnostics;
-using MongoDB.Bson;
-using MongoDB.Driver;
 using Discord;
 using Discord.Interactions;
 using GroundedBot.Services;
 
 namespace GroundedBot.Commands
 {
-    [Group("dev", "[DEV] Fejlesztő parancsok")]
-    [RequireOwner]
-    public class Dev : InteractionModuleBase
-    {
-        public MongoService _mongo { get; set; }
+	[Group("dev", "[DEV] Fejlesztő parancsok")]
+	[RequireOwner]
+	public class Dev : InteractionModuleBase
+	{
+		public MongoService _mongo { get; set; }
 
-        [SlashCommand("export", "[DEV] Adatbázisok exportálása")]
-        public async Task Export()
-        {
-            List<FileAttachment> temp = new();
-            string classes = _mongo.Classes.AsQueryable().ToArray().ToJson();
-            File.WriteAllText("classes.json", classes);
-            temp.Add(new FileAttachment("classes.json"));
-            string guilds = _mongo.Guilds.AsQueryable().ToArray().ToJson();
-            File.WriteAllText("guilds.json", guilds);
-            temp.Add(new FileAttachment("guilds.json"));
-            await RespondWithFilesAsync(temp);
-            await RespondAsync();
-        }
+		[SlashCommand("restart", "[DEV] Bot újraindítása")]
+		public async Task Restart()
+		{
+			try
+			{
+				var process = new ProcessStartInfo
+				{
+					FileName = "/bin/bash",
+					Arguments = $"-c \"" +
+						"cd ..\n" +
+						"sudo git pull\n" +
+						"sudo dotnet build -c Release -o build\n" +
+						"cd build\n" +
+						"sudo dotnet GroundedBot.dll" +
+						"\"",
+					RedirectStandardOutput = true,
+					UseShellExecute = false,
+					CreateNoWindow = true
+				};
+				Process.Start(process);
+				await RespondAsync(embed: EmbedService.Info("Újraindítás...", "Ez eltarthat egy darabig."));
+				Environment.Exit(0);
+			}
+			catch (Exception e)
+			{
+				await RespondAsync(
+					embed: EmbedService.Error(
+						"Hiba",
+						$"Nem található bash a `/bin/bash` helyen.\n" +
+						$"```{e.Message}```"),
+					ephemeral: true);
+			}
+		}
 
-        [SlashCommand("import", "[DEV] Adatbázisok importálása")]
-        public async Task Import()
-        {
+		[SlashCommand("shutdown", "[DEV] Bot leállítása")]
+		public async Task Shutdown()
+		{
+			await RespondAsync(embed: EmbedService.Info("Kikapcsolás..."));
+			Environment.Exit(0);
+		}
 
-        }
-
-        [SlashCommand("restart", "[DEV] Bot újraindítása")]
-        public async Task Restart()
-        {
-            try
-            {
-                var process = new ProcessStartInfo
-                {
-                    FileName = "/bin/bash",
-                    Arguments = $"-c \"" +
-                        "cd ..\n" +
-                        "sudo git pull\n" +
-                        "sudo dotnet build -c Release -o build\n" +
-                        "cd build\n" +
-                        "sudo dotnet GroundedBot.dll" +
-                        "\"",
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                };
-                Process.Start(process);
-                await RespondAsync(embed: EmbedService.Info("Újraindítás...", "Ez eltarthat egy darabig."));
-                Environment.Exit(0);
-            }
-            catch (Exception e)
-            {
-                await RespondAsync(
-                    embed: EmbedService.Error(
-                        "Hiba",
-                        $"Nem található bash a `/bin/bash` helyen.\n" +
-                        $"```{e.Message}```"),
-                    ephemeral: true);
-            }
-        }
-
-        [SlashCommand("shutdown", "[DEV] Bot leállítása")]
-        public async Task Shutdown()
-        {
-            await RespondAsync(embed: EmbedService.Info("Kikapcsolás..."));
-            Environment.Exit(0);
-        }
-
-        [SlashCommand("Test", "[DEV] Tesztelés")]
-        public async Task Test()
-        {
-            await RespondAsync(embed: EmbedService.Info("Kikapcsolás..."));
-            Environment.Exit(0);
-        }
-    }
+		[SlashCommand("test", "[DEV] Tesztelés")]
+		public async Task Test()
+		{
+			await RespondAsync(Context.User.Mention, allowedMentions: AllowedMentions.None);
+		}
+	}
 }

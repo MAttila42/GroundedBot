88d863391258d7df9bcdae3d2bc459f144e2a1e7
ExAtom <menyhart.atika@gmail.com>
2022-11-07 08:51:28 +0100

    v2.0.13 - PingRequest fix

--- Commands/Dev.cs
+++ Commands/Dev.cs
@@ -1,61 +1,67 @@
 ﻿using System.Diagnostics;
 using Discord;
 using Discord.Interactions;
+using Discord.WebSocket;
 using GroundedBot.Services;
 
 namespace GroundedBot.Commands
 {
-	[Group("dev", "[DEV] Fejlesztő parancsok")]
-	[RequireOwner]
-	public class Dev : InteractionModuleBase
-	{
-		public MongoService _mongo { get; set; }
+    [Group("dev", "[DEV] Fejlesztő parancsok")]
+    [RequireOwner]
+    public class Dev : InteractionModuleBase
+    {
+        public DiscordSocketClient _client { get; set; }
+        public MongoService _mongo { get; set; }
 
-		[SlashCommand("restart", "[DEV] Bot újraindítása")]
-		public async Task Restart()
-		{
-			try
-			{
-				var process = new ProcessStartInfo
-				{
-					FileName = "/bin/bash",
-					Arguments = $"-c \"" +
-						"cd ..\n" +
-						"sudo git pull\n" +
-						"sudo dotnet build -c Release -o build\n" +
-						"cd build\n" +
-						"sudo dotnet GroundedBot.dll" +
-						"\"",
-					RedirectStandardOutput = true,
-					UseShellExecute = false,
-					CreateNoWindow = true
-				};
-				Process.Start(process);
-				await RespondAsync(embed: EmbedService.Info("Újraindítás...", "Ez eltarthat egy darabig."));
-				Environment.Exit(0);
-			}
-			catch (Exception e)
-			{
-				await RespondAsync(
-					embed: EmbedService.Error(
-						"Hiba",
-						$"Nem található bash a `/bin/bash` helyen.\n" +
-						$"```{e.Message}```"),
-					ephemeral: true);
-			}
-		}
+        [SlashCommand("restart", "[DEV] Bot újraindítása")]
+        public async Task Restart()
+        {
+            try
+            {
+                var process = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"" +
+                        "cd ..\n" +
+                        "sudo git pull\n" +
+                        "sudo dotnet build -c Release -o build\n" +
+                        "cd build\n" +
+                        "sudo dotnet GroundedBot.dll" +
+                        "\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                Process.Start(process);
+                await RespondAsync(embed: EmbedService.Info("Újraindítás...", "Ez eltarthat egy darabig."));
+                Environment.Exit(0);
+            }
+            catch (Exception e)
+            {
+                await RespondAsync(
+                    embed: EmbedService.Error(
+                        "Hiba",
+                        $"Nem található bash a `/bin/bash` helyen.\n" +
+                        $"```{e.Message}```"),
+                    ephemeral: true);
+            }
+        }
 
-		[SlashCommand("shutdown", "[DEV] Bot leállítása")]
-		public async Task Shutdown()
-		{
-			await RespondAsync(embed: EmbedService.Info("Kikapcsolás..."));
-			Environment.Exit(0);
-		}
+        [SlashCommand("guildcount", "[DEV] Szerverek száma")]
+        public async Task ServerCount() =>
+            await RespondAsync(embed: EmbedService.Info($"A bot {_client.Guilds.Count} db szerveren van bent."));
 
-		[SlashCommand("test", "[DEV] Tesztelés")]
-		public async Task Test()
-		{
-			await RespondAsync(Context.User.Mention, allowedMentions: AllowedMentions.None);
-		}
-	}
+        [SlashCommand("shutdown", "[DEV] Bot leállítása")]
+        public async Task Shutdown()
+        {
+            await RespondAsync(embed: EmbedService.Info("Kikapcsolás..."));
+            Environment.Exit(0);
+        }
+
+        [SlashCommand("test", "[DEV] Tesztelés")]
+        public async Task Test()
+        {
+            await RespondAsync(Context.User.Mention, allowedMentions: AllowedMentions.None);
+        }
+    }
 }
--- GroundedBot.cs
+++ GroundedBot.cs
@@ -8,96 +8,96 @@ using GroundedBot.Services;
 
 namespace GroundedBot
 {
-	class GroundedBot
-	{
-		private readonly Config _config;
-		private readonly DiscordSocketClient _client;
-		private readonly InteractionService _interaction;
-		private readonly EventService _event;
-		private readonly EmojiService _emoji;
-		private readonly MongoService _mongo;
+    class GroundedBot
+    {
+        private readonly Config _config;
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _interaction;
+        private readonly EventService _event;
+        private readonly EmojiService _emoji;
+        private readonly MongoService _mongo;
 
-		private readonly IServiceProvider _services;
+        private readonly IServiceProvider _services;
 
-		public GroundedBot(Config config)
-		{
-			this._config = config;
-			this._client = new DiscordSocketClient(new DiscordSocketConfig
-			{
-				AlwaysDownloadUsers = true,
-				GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMembers | GatewayIntents.GuildMessages,
-				UseInteractionSnowflakeDate = false
-			});
-			this._interaction = new InteractionService(_client.Rest);
-			this._event = new();
-			this._emoji = new();
-			this._mongo = new MongoService("mongodb://localhost:27017");
+        public GroundedBot(Config config)
+        {
+            this._config = config;
+            this._client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                AlwaysDownloadUsers = true,
+                GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMembers | GatewayIntents.GuildMessages | GatewayIntents.MessageContent,
+                UseInteractionSnowflakeDate = false
+            });
+            this._interaction = new InteractionService(_client.Rest);
+            this._event = new();
+            this._emoji = new();
+            this._mongo = new MongoService("mongodb://localhost:27017");
 
-			this._services = new ServiceCollection()
-				.AddSingleton(_config)
-				.AddSingleton(_client)
-				.AddSingleton(_emoji)
-				.AddSingleton(_mongo)
-				.BuildServiceProvider();
-		}
+            this._services = new ServiceCollection()
+                .AddSingleton(_config)
+                .AddSingleton(_client)
+                .AddSingleton(_emoji)
+                .AddSingleton(_mongo)
+                .BuildServiceProvider();
+        }
 
-		public async Task MainAsync()
-		{
-			_client.Log += Log;
-			_interaction.Log += Log;
+        public async Task MainAsync()
+        {
+            _client.Log += Log;
+            _interaction.Log += Log;
 
-			await _client.LoginAsync(TokenType.Bot, this._config.Token);
-			await _client.StartAsync();
+            await _client.LoginAsync(TokenType.Bot, this._config.Token);
+            await _client.StartAsync();
 
-			_client.SlashCommandExecuted += ExecuteInteractionAsync;
-			_client.ButtonExecuted += ExecuteInteractionAsync;
-			_client.ModalSubmitted += ExecuteInteractionAsync;
+            _client.SlashCommandExecuted += ExecuteInteractionAsync;
+            _client.ButtonExecuted += ExecuteInteractionAsync;
+            _client.ModalSubmitted += ExecuteInteractionAsync;
 
-			_client.MessageReceived += async m =>
-			{
-				if (m.Author.IsBot) return;
-				SocketCommandContext ctx = new(_client, m as SocketUserMessage);
-				await _event.ExecuteMessageEventsAsync(ctx, _services);
-			};
+            _client.MessageReceived += async m =>
+            {
+                if (m.Author.IsBot) return;
+                SocketCommandContext ctx = new(_client, m as SocketUserMessage);
+                await _event.ExecuteMessageEventsAsync(ctx, _services);
+            };
 
-			_client.Ready += async () =>
-			{
-				_emoji.LoadEmojis(_client, _config.EmojiGuilds);
-				await _mongo.UpdateGuilds(_client.Guilds);
+            _client.Ready += async () =>
+            {
+                _emoji.LoadEmojis(_client, _config.EmojiGuilds);
+                await _mongo.UpdateGuilds(_client.Guilds);
 
-				foreach (SocketGuild guild in _client.Guilds)
-					foreach (ulong userId in _mongo.Classes.AsQueryable().SelectMany(c => c.Students))
-					{
-						SocketGuildUser user = guild.GetUser(userId);
-						if (user == null)
-							await _mongo.Classes.UpdateManyAsync(c => c.Students.Contains(userId) && c.Guild == guild.Id, Builders<TanClass>.Update.Pull(c => c.Students, userId));
-					}
+                foreach (SocketGuild guild in _client.Guilds)
+                    foreach (ulong userId in _mongo.Classes.AsQueryable().SelectMany(c => c.Students))
+                    {
+                        SocketGuildUser user = guild.GetUser(userId);
+                        if (user == null)
+                            await _mongo.Classes.UpdateManyAsync(c => c.Students.Contains(userId) && c.Guild == guild.Id, Builders<TanClass>.Update.Pull(c => c.Students, userId));
+                    }
 
-				await _interaction.AddModulesAsync(typeof(GroundedBot).Assembly, _services);
+                await _interaction.AddModulesAsync(typeof(GroundedBot).Assembly, _services);
 #if DEBUG
-				await _interaction.RegisterCommandsToGuildAsync(_config.DebugGuild, true);
+                await _interaction.RegisterCommandsToGuildAsync(_config.DebugGuild, true);
 #else
                 await _interaction.RegisterCommandsGloballyAsync(true);
 #endif
-			};
+            };
 
-			_client.JoinedGuild += async g => await _mongo.UpdateGuilds(_client.Guilds);
-			_client.UserLeft += async (g, u) => await _mongo.Classes.UpdateManyAsync(c => c.Students.Contains(u.Id) && c.Guild == g.Id, Builders<TanClass>.Update.Pull(c => c.Students, u.Id));
+            _client.JoinedGuild += async g => await _mongo.UpdateGuilds(_client.Guilds);
+            _client.UserLeft += async (g, u) => await _mongo.Classes.UpdateManyAsync(c => c.Students.Contains(u.Id) && c.Guild == g.Id, Builders<TanClass>.Update.Pull(c => c.Students, u.Id));
 
-			await Task.Delay(-1).ConfigureAwait(false);
-		}
+            await Task.Delay(-1).ConfigureAwait(false);
+        }
 
-		private Task Log(LogMessage msg)
-		{
-			Console.WriteLine(msg.ToString());
-			return Task.CompletedTask;
-		}
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
 
-		private async Task ExecuteInteractionAsync<T>(T i)
-			where T : SocketInteraction
-		{
-			SocketInteractionContext<T> ctx = new SocketInteractionContext<T>(_client, i);
-			await _interaction.ExecuteCommandAsync(ctx, _services);
-		}
-	}
+        private async Task ExecuteInteractionAsync<T>(T i)
+            where T : SocketInteraction
+        {
+            SocketInteractionContext<T> ctx = new SocketInteractionContext<T>(_client, i);
+            await _interaction.ExecuteCommandAsync(ctx, _services);
+        }
+    }
 }
